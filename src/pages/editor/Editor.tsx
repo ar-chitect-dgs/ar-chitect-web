e80bac204fc5bd8ffd1e1a2d29a7a87dab0585fe
jdudz <jdudzinska625@gmail.com>
2024-10-27 16:55:54 +0100

    Manual fixes

--- src/pages/editor/Editor.tsx
+++ src/pages/editor/Editor.tsx
@@ -1,30 +1,35 @@
 /* eslint-disable react/no-unknown-property */
-import { Canvas, MeshProps, useFrame } from "@react-three/fiber";
-import { useEffect, useRef } from "react";
-import { useSelector } from "react-redux";
+import { Canvas, MeshProps, useFrame } from '@react-three/fiber';
+import { useEffect, useRef } from 'react';
+import { useSelector } from 'react-redux';
 import * as THREE from 'three';
-import { sceneSelector, changeHoveredState, changeActiveState } from "../../redux/slices/scene";
-import { useAppDispatch } from "../../redux";
+import {
+  sceneSelector,
+  changeHoveredState,
+  changeActiveState,
+} from '../../redux/slices/scene';
+import { useAppDispatch } from '../../redux';
 
-function Box(props: MeshProps) {
-  const meshRef = useRef<THREE.Mesh>(null!)
-  const { hovered, active } = useSelector(sceneSelector)
+function Box({ position }: MeshProps) {
+  const meshRef = useRef<THREE.Mesh>(null!);
+  const { hovered, active } = useSelector(sceneSelector);
   const dispatch = useAppDispatch();
 
-  useFrame((state, delta) => (meshRef.current.rotation.x += delta))
+  // eslint-disable-next-line no-return-assign
+  useFrame((_state, delta) => (meshRef.current.rotation.x += delta));
 
-  const color = hovered ? 'hotpink' : '#2f74c0'
+  const color = hovered ? 'hotpink' : '#2f74c0';
   const scale = active ? 1.5 : 1;
 
   useEffect(() => {
     if (meshRef.current) {
-      (meshRef.current.material as THREE.MeshStandardMaterial).color.set(color)
+      (meshRef.current.material as THREE.MeshStandardMaterial).color.set(color);
     }
-  }, [hovered])
+  }, [hovered]);
 
   return (
     <mesh
-      position={props.position}
+      position={position}
       ref={meshRef}
       scale={scale}
       onClick={() => dispatch(changeActiveState())}
@@ -34,18 +39,30 @@ function Box(props: MeshProps) {
       <boxGeometry args={[1, 1, 1]} />
       <meshStandardMaterial color={color} />
     </mesh>
-  )
+  );
 }
 
-function Editor() {
+function Editor(): JSX.Element {
   return (
     <div>
       <Canvas
-        camera={{ fov: 45, near: 0.1, far: 2000, position: [4, 3, 10] }}
+        camera={{
+          fov: 45,
+          near: 0.1,
+          far: 2000,
+          position: [4, 3, 10],
+        }}
         gl={{ antialias: true }}
-        scene={{}}>
+        scene={{}}
+      >
         <ambientLight intensity={Math.PI / 2} />
-        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+        <spotLight
+          position={[10, 10, 10]}
+          angle={0.15}
+          penumbra={1}
+          decay={0}
+          intensity={Math.PI}
+        />
         <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
         <Box position={[1, 1, 1]} />
       </Canvas>
@@ -53,4 +70,4 @@ function Editor() {
   );
 }
 
-export default Editor
+export default Editor;
--- src/redux/index.tsx
+++ src/redux/index.tsx
@@ -1,16 +1,16 @@
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 
-import sceneReducer from "./slices/scene";
-import { useDispatch } from "react-redux";
+import { useDispatch } from 'react-redux';
+import sceneReducer from './slices/scene';
 
 const rootReducer = combineReducers({
   sceneReducer,
-})
-export type RootState = ReturnType<typeof rootReducer>
+});
+export type RootState = ReturnType<typeof rootReducer>;
 
-export default rootReducer
+export default rootReducer;
 
-export const _store = configureStore({ reducer: rootReducer })
+export const store = configureStore({ reducer: rootReducer });
 
-export type AppDispatch = typeof _store.dispatch
-export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
+export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
