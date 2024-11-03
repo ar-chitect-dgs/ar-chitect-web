300176fca2356b769bbf4634e823d760b71f9f87
Julia <65892459+andrmdmd@users.noreply.github.com>
2024-11-03 15:35:07 +0100

    Update src/App.tsx
    Co-authored-by: Marcin Słupczyński <132074948+mslup@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import Profile from './pages/Profile';
 function App(): JSX.Element {
   return (
     <Router>
-      <div style={{ display: 'flex' }}>
+      <div style={{ display: 'flex', height: '100vh' }}>
         <Sidebar />
         <div style={{ flexGrow: 1, padding: '16px' }}>
           <Routes>
