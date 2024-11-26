5c568b2f339117478a216d0886a569fa806267f8
Marcin Słupczyński <marcinxslup@gmail.com>
2024-11-26 22:21:02 +0100

    Changed canvas component

--- src/feature/creator/viewport/Viewport.tsx
+++ src/feature/creator/viewport/Viewport.tsx
@@ -1,48 +1,21 @@
 /* eslint-disable react/no-unknown-property */
 /* eslint-disable react/jsx-props-no-spreading */
-import { Grid } from '@react-three/drei';
-import { Canvas } from '@react-three/fiber';
-
-function Ground() {
-  const gridConfig = {
-    cellSize: 0.25,
-    cellThickness: 1,
-    cellColor: '#6f6f6f',
-    sectionSize: 0.5,
-    sectionThickness: 1,
-    sectionColor: '#595959',
-    fadeStrength: 1,
-    followCamera: false,
-    infiniteGrid: true,
-  };
-  return <Grid position={[0, 0, 0]} {...gridConfig} />;
-}
+import { useRef, useEffect } from 'react';
 
 function CreatorViewport(): JSX.Element {
-  return (
-    <Canvas
-      camera={{
-        fov: 45,
-        near: 0.1,
-        far: 2000,
-        position: [0, 10, 0],
-        rotation: [-Math.PI / 2, 0, 0],
-      }}
-      gl={{ antialias: true }}
-      scene={{}}
-    >
-      <ambientLight intensity={Math.PI / 2} />
-      <spotLight
-        position={[10, 10, 10]}
-        angle={0.15}
-        penumbra={1}
-        decay={0}
-        intensity={Math.PI}
-      />
-      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-      <Ground />
-    </Canvas>
-  );
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (canvas === null) { return; }
+    const context = canvas.getContext('2d');
+
+    if (context === null) { return; }
+    context.fillStyle = '#000000';
+    context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+  }, []);
+
+  return <canvas ref={canvasRef} />;
 }
 
 export default CreatorViewport;
