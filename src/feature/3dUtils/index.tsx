17c72b26156df05221374a0f47a3793b6c6070cd
Marcin Slupczynski <marcinxslup@gmail.com>
2024-12-05 19:46:00 +0100

    Fix imports 2

---
+++ src/feature/3dUtils/index.tsx
@@ -0,0 +1,6 @@
+export { Floor } from './Floor';
+export { Ground } from './Ground';
+export { Model } from './Model';
+export { RoomCorners as Vertices } from './RoomCorners';
+export { Sphere, SphereType } from './Sphere';
+export { Walls } from './Walls';
