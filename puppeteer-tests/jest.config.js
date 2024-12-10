08b733eba308aa9c515e0e709c47350926729c44
Julia <jdudzinska625@gmail.com>
2024-12-10 19:40:28 +0100

    Delete comment

--- puppeteer-tests/jest.config.js
+++ puppeteer-tests/jest.config.js
@@ -1,12 +1,3 @@
-// module.exports = {
-//   preset: 'jest-puppeteer',
-//   transform: {
-//     '^.+\\.tsx?$': 'ts-jest',
-//   },
-//   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
-//   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-// };
-
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
