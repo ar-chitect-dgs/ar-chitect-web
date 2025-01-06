ad2e48d9d66ad9cdd8fcc54121c9cdbdbe425044
Marcin Słupczyński <132074948+mslup@users.noreply.github.com>
2025-01-06 19:44:29 +0100

    Update src/feature/editor/viewport/Viewport.tsx

--- src/feature/editor/viewport/Viewport.tsx
+++ src/feature/editor/viewport/Viewport.tsx
@@ -65,7 +65,6 @@ function EditorViewport(): JSX.Element {
 
         <InteractiveScene />
         <ContactShadows scale={10} blur={3} opacity={0.25} far={10} />
-        {/* <Stats /> */}
       </Canvas>
     </Suspense>
   );
