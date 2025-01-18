b2dc30e56e1496b6e99887aeb588a6222e2bbe3d
Marcin Słupczyński <marcinxslup@gmail.com>
2025-01-18 11:05:32 +0100

    oop

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import Editor from './feature/editor/Editor';
+import AuthProvider from './auth/AuthProvider';
+import AppRouter from './feature/navigation/AppRouter';
 import './firebaseConfig';
 
 function App(): JSX.Element {
   return (
-    <Editor />
-    // <AuthProvider>
-    //   <AppRouter />
-    // </AuthProvider>
+    <AuthProvider>
+      <AppRouter />
+    </AuthProvider>
   );
 }
 
