e262410e068ec328ccd73aa536825b092f0b35ce
Marcin Slupczynski <marcinxslup@gmail.com>
2025-01-22 18:04:37 +0100

    godf

--- src/feature/3dUtils/Model.tsx
+++ src/feature/3dUtils/Model.tsx
@@ -1,5 +1,4 @@
 /* eslint-disable react/no-unknown-property */
-import { Outlines } from '@react-three/drei';
 import { useEffect, useRef, useState } from 'react';
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
