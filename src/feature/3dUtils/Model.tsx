190fad39921d695cd1b1eb243086945faaf864b2
Marcin Słupczyński <marcinxslup@gmail.com>
2025-01-25 00:27:00 +0100

    bug fix

--- src/feature/3dUtils/Model.tsx
+++ src/feature/3dUtils/Model.tsx
@@ -21,6 +21,8 @@ export function Model({
   passDepthToParent,
 }: ModelProps): JSX.Element {
   const [gltfModel, setGltfModel] = useState<THREE.Group | null>(null);
+  // Axis aligned copy of the model to compute bounding box
+  const [aaGltfModel, setAaGltfModel] = useState<THREE.Group | null>(null);
   const [depth, setDepth] = useState(0);
   const [height, setHeight] = useState(0);
   const [width, setWidth] = useState(0);
@@ -33,8 +35,9 @@ export function Model({
     loader.load(
       url,
       (gltf) => {
-        const clonedScene = gltf.scene.clone(true);
-        setGltfModel(clonedScene);
+        const model = gltf.scene.clone(true);
+        setGltfModel(model);
+        setAaGltfModel(model.clone(true));
       },
       undefined,
       (error) => {
@@ -46,7 +49,7 @@ export function Model({
   useEffect(() => {
     if (!meshRef.current) return;
 
-    const boundingBox = new THREE.Box3().setFromObject(meshRef.current as THREE.Object3D);
+    const boundingBox = new THREE.Box3().setFromObject(meshRef.current as THREE.Object3D, true);
 
     setDepth(boundingBox.max.x - boundingBox.min.x);
     setHeight(boundingBox.max.y - boundingBox.min.y);
@@ -66,29 +69,34 @@ export function Model({
     opacity = 0.2;
   }
 
-  if (!gltfModel) return <mesh />;
+  if (!gltfModel || !aaGltfModel) return <mesh />;
 
   return (
-    <mesh
-      position={[position.x, position.y, position.z]}
-      rotation={[rotation.x, rotation.y, rotation.z]}
-    >
-      <mesh ref={meshRef}>
-        <primitive object={gltfModel} />
-      </mesh>
+    <group>
       <mesh
-        position={[0, height / 2, 0]}
-        userData={{ name: MODEL, id: inProjectId }}
-        castShadow={false}
+        position={[position.x, position.y, position.z]}
+        rotation={[rotation.x, rotation.y, rotation.z]}
       >
-        <boxGeometry args={[depth + 0.1, height + 0.1, width + 0.1]} />
-        <meshStandardMaterial
-          color="lightblue"
-          transparent
-          opacity={opacity}
-          visible={hovered || active}
-        />
+        <mesh>
+          <primitive object={gltfModel} />
+        </mesh>
+        <mesh
+          position={[0, height / 2, 0]}
+          userData={{ name: MODEL, id: inProjectId }}
+          castShadow={false}
+        >
+          <boxGeometry args={[depth + 0.1, height + 0.1, width + 0.1]} />
+          <meshStandardMaterial
+            color="lightblue"
+            transparent
+            opacity={opacity}
+            visible={hovered || active}
+          />
+        </mesh>
       </mesh>
-    </mesh>
+      <mesh position={[position.x, position.y, position.z]} ref={meshRef} visible={false}>
+        <primitive object={aaGltfModel} />
+      </mesh>
+    </group>
   );
 }
