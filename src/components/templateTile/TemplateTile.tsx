aec116ee5002f4ec6f7728363c56be263db07e7a
Julia <jdudzinska625@gmail.com>
2025-01-27 00:23:19 +0100

    Formatting pt 4

--- src/components/templateTile/TemplateTile.tsx
+++ src/components/templateTile/TemplateTile.tsx
@@ -1,3 +1,4 @@
+import { useTranslation } from 'react-i18next';
 import { ApiProject } from '../../api/projects/types';
 import placeholder from '../../assets/placeholder_project.png';
 import Card from '../card/Card';
@@ -8,32 +9,33 @@ interface TemplateTileProps {
   onClick: () => void;
 }
 
-const TemplateTile = ({ project, onClick }: TemplateTileProps): JSX.Element => (
-  <Card className="project-card">
-    <div
-      onClick={onClick}
-      role="button"
-      tabIndex={0}
-      className="project-wrapper"
-    >
-      <img
-        className="project-thumbnail"
-        src={project.thumb || placeholder}
-        alt={`${project.projectName} thumbnail`}
-      />
-      <div className="project-details">
-        <div className="project-header">
-          <span className="project-name">{project.projectName}</span>
-        </div>
-        <div className="project-objects">
-          {/* todo localize */}
-          Consists of:
-          {' '}
-          {project.objects.map((obj) => obj.name ?? obj.id).join(', ')}
+const TemplateTile = ({ project, onClick }: TemplateTileProps): JSX.Element => {
+  const { t } = useTranslation();
+  return (
+    <Card className="project-card">
+      <div
+        onClick={onClick}
+        role="button"
+        tabIndex={0}
+        className="project-wrapper"
+      >
+        <img
+          className="project-thumbnail"
+          src={project.thumb || placeholder}
+          alt={`${project.projectName} thumbnail`}
+        />
+        <div className="project-details">
+          <div className="project-header">
+            <span className="project-name">{project.projectName}</span>
+          </div>
+          <div className="project-objects">
+            {t('templates.consistsOf')}
+            {project.objects.map((obj) => obj.name ?? obj.id).join(', ')}
+          </div>
         </div>
       </div>
-    </div>
-  </Card>
-);
+    </Card>
+  );
+};
 
 export default TemplateTile;
--- src/pages/Templates.tsx
+++ src/pages/Templates.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { Scrollbars } from 'react-custom-scrollbars-2';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 import { fetchAllTemplates } from '../api/projects';
 import { ApiProject } from '../api/projects/types';
 import { useAuth } from '../auth/AuthProvider';
@@ -13,12 +14,19 @@ import { setScene } from '../redux/slices/editor';
 import { mapApiProjectToProjectScene } from '../utils/mappers';
 import './styles/Projects.css';
 
+const TEMPLATES_SECTIONS = {
+  Kitchen: 'Kitchen',
+  Bathroom: 'Bathroom',
+  Bedroom: 'Bedroom / Living room',
+};
+
 interface ProjectSection {
   sectionName: string;
   projects: ApiProject[];
 }
 
 const Templates = (): JSX.Element => {
+  const { t } = useTranslation();
   const [sections, setSections] = useState<ProjectSection[]>([]);
   const [loading, setLoading] = useState(true);
   const { user } = useAuth();
@@ -29,7 +37,7 @@ const Templates = (): JSX.Element => {
     const fetchProjects = async () => {
       if (user) {
         try {
-          const data = await fetchAllTemplates(['Kitchen', 'Bathroom', 'Bedroom']);
+          const data = await fetchAllTemplates(Object.keys(TEMPLATES_SECTIONS));
           console.log(data);
           setSections(data);
         } catch (error) {
@@ -47,16 +55,19 @@ const Templates = (): JSX.Element => {
 
   if (loading) {
     /* todo localize */
-    message = <div className="projects-message">Loading templates...</div>;
+    message = <div className="projects-message">{t('templates.loadingTemplates')}</div>;
   } else if (sections.length === 0) {
-    message = <div className="projects-message">No templates found.</div>;
+    message = <div className="projects-message">{t('templates.noTemplates')}</div>;
   }
 
   const handleProjectClick = async (apiProject: ApiProject) => {
     console.log(`Navigating to project with ID: ${apiProject.id}`);
 
     try {
-      const { project, scene } = await mapApiProjectToProjectScene(apiProject, true);
+      const { project, scene } = await mapApiProjectToProjectScene(
+        apiProject,
+        true,
+      );
       dispatch(setScene(scene));
       dispatch(setProject(project));
       navigate(ROUTES.EDITOR);
@@ -72,30 +83,36 @@ const Templates = (): JSX.Element => {
         <ScrollBar className="scrollbar">
           <div className="scrollbar-inside-container">
             {!loading
-            && sections.map((section) => (
-              <div key={section.sectionName} className="template-section">
-                <h2 className="section-title">{section.sectionName}</h2>
-                <Scrollbars
-                  className="templates-scrollbar"
-                  renderTrackHorizontal={(props) => (
-                    <div {...props} className="scrollbar-track-horizontal" />
-                  )}
-                  renderThumbHorizontal={(props) => (
-                    <div {...props} className="scrollbar-thumb-horizontal" />
-                  )}
-                >
-                  <div className="templates-container">
-                    {section.projects.map((project) => (
-                      <TemplateTile
-                        key={project.id}
-                        project={project}
-                        onClick={() => handleProjectClick(project)}
-                      />
-                    ))}
-                  </div>
-                </Scrollbars>
-              </div>
-            ))}
+              && sections.map((section) => (
+                <div key={section.sectionName} className="template-section">
+                  <h2 className="section-title">
+                    {
+                      TEMPLATES_SECTIONS[
+                        section.sectionName as keyof typeof TEMPLATES_SECTIONS
+                      ]
+                    }
+                  </h2>
+                  <Scrollbars
+                    className="templates-scrollbar"
+                    renderTrackHorizontal={(props) => (
+                      <div {...props} className="scrollbar-track-horizontal" />
+                    )}
+                    renderThumbHorizontal={(props) => (
+                      <div {...props} className="scrollbar-thumb-horizontal" />
+                    )}
+                  >
+                    <div className="templates-container">
+                      {section.projects.map((project) => (
+                        <TemplateTile
+                          key={project.id}
+                          project={project}
+                          onClick={() => handleProjectClick(project)}
+                        />
+                      ))}
+                    </div>
+                  </Scrollbars>
+                </div>
+              ))}
           </div>
         </ScrollBar>
       </div>
